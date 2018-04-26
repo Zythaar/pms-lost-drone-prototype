f7e24c3b3c25d58e37d9df550aa33c275fe1426e
Zythaar <stefangreiss@gmail.com>
2018-04-26 15:48:04 +0200

    Player Controller implemented
    Basic movement
    Core functionality scripts added

---
+++ PMD Lost Drone prototype/Assets/Scripts/TopDownScroller/Input/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax),
+            0.0f
+        );
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Vector3 topLeft = new Vector3(boundary.xMin, boundary.yMax);
+        Vector3 topRight = new Vector3(boundary.xMax, boundary.yMax);
+        Vector3 bottomLeft = new Vector3(boundary.xMin, boundary.yMin);
+        Vector3 bottomRight = new Vector3(boundary.xMax, boundary.yMin);
+
+        Gizmos.DrawLine(bottomLeft, topLeft);//left
+        Gizmos.DrawLine(topRight, topLeft);//top
+        Gizmos.DrawLine(bottomRight, topRight);//right
+        Gizmos.DrawLine(bottomRight, bottomLeft);//bottom
+    }
+}
---
+++ PMD Lost Drone prototype/Assets/Scripts/TopDownScroller/Input/TopDownScrollerKeyboardMouseInput.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityInput = UnityEngine.Input;
+
+namespace Core.Input
+{
+
+    public class TopDownScrollerKeyboardMouseInput : MonoBehaviour
+    {
+
+    }
+}
